5c1debcad88be4bbc4652c72532a11413378c94c
juan <juanjomoss@gmail.com>
2025-03-20 11:36:37 -0500

    initial code

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2024,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "eqeqeq": "error",
+      "curly": "error",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "indent": ["error", 2],
+      "no-trailing-spaces": "error",
+      "arrow-spacing": ["error", { "before": true, "after": true }],
+      "prefer-const": "error",
+      "no-var": "error"
+    },
+  },
+)
---
+++ src/types/reactTypes/NavItem.ts
@@ -0,0 +1,5 @@
+export default interface NavItem {
+    title: string
+    href: string
+    icon: React.ReactNode
+}
