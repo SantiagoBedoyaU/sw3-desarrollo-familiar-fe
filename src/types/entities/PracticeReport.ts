715aabe9b99ba077dc0776688e37e00735739851
juan <juanjomoss@gmail.com>
2025-04-05 16:33:35 -0500

    prettier

--- src/types/entities/PracticeReport.ts
+++ src/types/entities/PracticeReport.ts
@@ -1,17 +1,17 @@
-import Article from './Article'
+import Article from "./Article";
 
 export default interface PracticeReport {
-    id?: string
-    title: string
-    authors: string
-    year: string
-    summary: string
-    keywords: string
-    thematicArea: string
-    file: File
+  id?: string;
+  title: string;
+  authors: string;
+  year: string;
+  summary: string;
+  keywords: string;
+  thematicArea: string;
+  file: File;
 
-    thematicArea2?: string
-    articleId?: string
+  thematicArea2?: string;
+  articleId?: string;
 
-    article?: Article
+  article?: Article;
 }
