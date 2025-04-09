ae2e152461701cc21d99de1d4b89e6be77f63741
juan <juanjomoss@gmail.com>
2025-04-09 09:17:51 -0500

    mocks strings

--- src/shared/types/mocks/ArticleMock.ts
+++ src/shared/types/mocks/ArticleMock.ts
@@ -4,11 +4,11 @@ const mockArticle: Article[] = [
   {
     _id: '1',
     title: 'Artículo de aceite enriquecido en niñas',
-    authors: 'Naydú Núñez',
+    authors: 'Naydú Núñez'.split(','),
     primaryThematicAxis:
       'La relación familia - escuela un asunto de corresponsabilidad',
     secondaryThematicAxis: 'Democratización de las relaciones familiares',
-    keywords: 'aceite, enriquecido, niñas',
+    keywords: 'aceite, enriquecido, niñas'.split(','),
     summary: 'Resumen del artículo',
     file: new File([], 'file.pdf'),
     year: '2023',
@@ -16,11 +16,11 @@ const mockArticle: Article[] = [
   {
     _id: '2',
     title: 'Artículo de aceite enriquecido en niñas',
-    authors: 'Naydú Núñez, Juan Perez',
+    authors: 'Naydú Núñez, Juan Perez'.split(','),
     primaryThematicAxis: 'Democratización de las relaciones familiares',
     secondaryThematicAxis:
       'La relación familia - escuela un asunto de corresponsabilidad',
-    keywords: 'aceite, enriquecido, niñas',
+    keywords: 'aceite, enriquecido, niñas'.split(','),
     summary: 'Resumen del artículo',
     file: new File([], 'file.pdf'),
     year: '2023',
@@ -28,11 +28,11 @@ const mockArticle: Article[] = [
   {
     _id: '3',
     title: 'Artículo de aceite enriquecido en niñas',
-    authors: 'Naydú Núñez',
+    authors: 'Naydú Núñez'.split(','),
     primaryThematicAxis:
       'La relación familia - escuela un asunto de corresponsabilidad',
     secondaryThematicAxis: 'Democratización de las relaciones familiares',
-    keywords: 'aceite, enriquecido, niñas',
+    keywords: 'aceite, enriquecido, niñas'.split(','),
     summary: 'Resumen del artículo',
     file: new File([], 'file.pdf'),
     year: '2023',
@@ -40,11 +40,11 @@ const mockArticle: Article[] = [
   {
     _id: '4',
     title: 'Artículo de aceite enriquecido en niñas',
-    authors: 'Naydú Núñez',
+    authors: 'Naydú Núñez'.split(','),
     primaryThematicAxis: 'Democratización de las relaciones familiares',
     secondaryThematicAxis:
       'La relación familia - escuela un asunto de corresponsabilidad',
-    keywords: 'aceite, enriquecido, niñas',
+    keywords: 'aceite, enriquecido, niñas'.split(','),
     summary: 'Resumen del artículo',
     file: new File([], 'file.pdf'),
     year: '2023',
